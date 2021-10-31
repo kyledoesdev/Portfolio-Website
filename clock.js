26fca95f8c1f87dee5c671615c6984f7a4fb0bfb
Kyle Evangelisto <kyleevangelisto@gmail.com>
2021-10-31 17:40:18 -0400

    clock is now my own, and better!

---
+++ clock.js
@@ -0,0 +1,30 @@
+var time;
+
+time = setInterval(updateTime, 1000);
+
+function updateTime() {
+  var clock = new Date();
+  var hours = clock.getHours();
+  var minutes = clock.getMinutes();
+  var seconds = clock.getSeconds();
+  var ampm = "AM";
+
+  if (hours > 12) {
+    ampm = "PM";
+    hours = hours - 12;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  document.getElementById("my_clock").innerHTML = hours + ":" + minutes + ":" + seconds + " " + ampm; 
+}
